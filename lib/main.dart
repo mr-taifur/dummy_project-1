341236661e52222980d04ebc818dc4f674c8cefa
mr-taifur <taifurshishir123@gmail.com>
2025-02-18 21:04:11 +0600

    Performance2

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,106 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LabClass05());
 }
 
-class MyApp extends StatelessWidget {
+class LabClass05 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter UI'),
-          centerTitle: true,
-          backgroundColor: Colors.amber,
+          title: Text("Lab 05 : Flutter"),
+          backgroundColor: Colors.orange,
+          
         ),
         body: Center(
-          child: ProductCard(),
+          child: ShoeCard(),
         ),
       ),
     );
   }
 }
 
-class ProductCard extends StatelessWidget {
+class ShoeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            blurRadius: 10,
-            spreadRadius: 2,
-          ),
-        ],
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Discount Label
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 0, 255, 217),
-              borderRadius: BorderRadius.circular(12),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 150,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: AssetImage("assets/shoe.webp"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 8,
+                  left: 8,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      "30% OFF",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            child: Text(
-              '50% FLAT OFF',
+            SizedBox(height: 10),
+            Text(
+              "Nike Air Shoes",
               style: TextStyle(
-                color: Colors.white,
+                fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          // Product Name
-          Text(
-            'NIKE AIR SHOES',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "\$450",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "\$500",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 5),
-          // Prices
-          Row(
-            children: [
-              Text(
-                '\$450',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(width: 30),
-              Text(
-                '\$900',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: const Color.fromARGB(255, 167, 17, 17),
-                  decoration: TextDecoration.lineThrough,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
