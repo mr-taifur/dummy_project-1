a53dee6225e1a65f084359f2eefaf1110f67b0d1
mr-taifur <taifurshishir123@gmail.com>
2025-02-18 12:55:34 +0600

    nikeShoesOff

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,98 @@
-// ignore_for_file: sized_box_for_whitespace
-
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 void main() {
-  runApp(const LabClass05());
+  runApp(MyApp());
 }
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Basic Flutter UI 02',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter"),
-            backgroundColor: Colors.blue,
-            centerTitle: true,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter UI'),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: ProductCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            blurRadius: 10,
+            spreadRadius: 2,
           ),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              height: 600,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  100,
-                  (index) => Icon(
-                    Iconsax.activity,
-                    size: index.toDouble(),
-                    color: const Color.fromARGB(255, 21, 10, 255),
-                  ),
-                ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Discount Label
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 0, 255, 217),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              '50% FLAT OFF',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-        ));
+          SizedBox(height: 20),
+          // Product Name
+          Text(
+            'NIKE AIR SHOES',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 5),
+          // Prices
+          Row(
+            children: [
+              Text(
+                '\$450',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(width: 30),
+              Text(
+                '\$900',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: const Color.fromARGB(255, 167, 17, 17),
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dummy_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Test());
+    await tester.pumpWidget(const LabClass05());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
