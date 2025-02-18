0e4d76b13b87b5e38126d1915493ad52ba1c6141
mr-taifur <taifurshishir123@gmail.com>
2025-02-18 10:48:21 +0600

    task2

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,20 @@ class LabClass05 extends StatelessWidget {
             backgroundColor: Colors.blue,
             centerTitle: true,
           ),
-          body: Row(
-            children: [
-              Icon(Icons.star,
-                  size: 50, color: const Color.fromARGB(210, 144, 0, 122)),
-              Icon(Icons.star,
-                  size: 50, color: const Color.fromARGB(210, 144, 0, 122)),
-              Icon(Icons.star,
-                  size: 50, color: const Color.fromARGB(210, 144, 0, 122)),
-              Icon(Icons.star,
-                  size: 50, color: const Color.fromARGB(210, 144, 0, 130)),
-              Icon(Icons.star,
-                  size: 50, color: const Color.fromARGB(41, 166, 131, 175)),
-            ],
+          body: Container(
+            height: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.contact_emergency,
+                    size: 50, color: const Color.fromARGB(210, 0, 68, 255)),
+                Icon(Icons.home,
+                    size: 50, color: const Color.fromARGB(210, 255, 0, 0)),
+                Icon(Icons.map,
+                    size: 50, color: const Color.fromARGB(210, 0, 255, 72)),
+              ],
+            ),
           ),
         ));
   }
